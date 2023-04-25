f390fc563f94005f56f57615654b67285cebe508
ligoulemarcos <97246648+ligoulemarcos@users.noreply.github.com>
2023-04-25 17:01:19 -0300

    viumi?

--- lib/src/viumi/viumi_checkout.dart
+++ lib/src/viumi/viumi_checkout.dart
@@ -1,3 +1,9 @@
-class ViumiCheckout {
+import "dart:async";
+import "dart:convert";
+import "package:http/http.dart" as http;
 
+class ViumiCheckout {
+  postViumiClient(Map<String, dynamic> data) async {
+    var response = await http.post(Uri.https("auth.geopagos.com", "/oauth/token"), body: json.encode(data));
+  }
 }
